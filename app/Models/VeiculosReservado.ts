053ad0e5a3fa7562bd3eb841627fb08e1b10a249
Gabriel Cruz <cruzg3dev@gmail.com>
2021-12-28 17:10:10 -0300

    name VeiculosReservado fixed

--- app/Models/VeiculosReservado.ts
+++ app/Models/VeiculosReservado.ts
@@ -1,7 +1,7 @@
 // import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
-export default class VeiculosRervado extends BaseModel {
+export default class VeiculosReservado extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
