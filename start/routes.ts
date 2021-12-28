98d032981a5f40486bfc1d819f7dfcbeb204e175
Gabriel Cruz <cruzg3dev@gmail.com>
2021-12-28 14:38:38 -0300

    org. rotas e autenticação

--- start/routes.ts
+++ start/routes.ts
@@ -6,12 +6,11 @@ Route.get('/', async () => {
 
 Route.post('/login', 'AuthController.login')
 
-Route.get('/logout', 'AuthController.logout')
+Route.post('/funcionarios', 'FuncionariosController.store')
 
 Route.group(() => {
   Route.group(() => {
     Route.get('/:?page=1&limit=&id=', 'FuncionariosController.index')
-    Route.post('', 'FuncionariosController.store')
     Route.put('/:id', 'FuncionariosController.update')
     Route.delete('/:id', 'FuncionariosController.destroy')
     // Route.get('/negociacoes/:id', 'FuncionariosController.show')
@@ -22,10 +21,15 @@ Route.group(() => {
     Route.get('', 'VeiculosController.index')
     Route.delete('/:id', 'VeiculosController.destroy')
     Route.put('/:id', 'VeiculosController.update')
+    //
   }).prefix('/veiculos')
 
   Route.group(() => {
     Route.put('/reservar/:id', 'ReservasController.update')
     Route.put('/vender/:id', 'VendasController.update')
+    //
   }).prefix('/veiculo/:veiculo_id/funcionario/:funcionario_id')
-})
+
+  Route.get('/logout', 'AuthController.logout')
+  //
+}).middleware('auth:api,api')
