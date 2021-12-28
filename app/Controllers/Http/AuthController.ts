1a6b927ae75f1211258a76f9b8e529a8182fde22
Gabriel Cruz <cruzg3dev@gmail.com>
2021-12-28 14:38:03 -0300

    autenticação funcional

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,23 +1,19 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Funcionario from 'App/Models/Funcionario'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
-import Hash from '@ioc:Adonis/Core/Hash'
-// import Auth from 'App/Models/Auth'
 
 export default class AuthController {
   public async login({ auth, request, response }: HttpContextContract) {
-    // const logar = await request.validate({
-    //   schema: schema.create({
-    //     email: schema.string({ trim: true }, [rules.email({ sanitize: true })]),
-    //     senha: schema.string(),
-    //   }),
-    //   messages: {
-    //     required: 'the field {{ field }} is required',
-    //   },
-    // })
-    // const { email, senha } = logar
-    const email = request.input('email')
-    const senha = request.input('senha')
+    const logar = await request.validate({
+      schema: schema.create({
+        email: schema.string({ trim: true }, [rules.email({ sanitize: true })]),
+        password: schema.string(),
+      }),
+      messages: {
+        required: 'the field {{ field }} is required',
+      },
+    })
+    const { email, password } = logar
     //
     try {
       const funcionario = await Funcionario.query()
@@ -25,8 +21,9 @@ export default class AuthController {
         .where('email', email)
         .select('id', 'cpf', 'nome', 'email', 'avatar', 'biografia')
 
-      const token = await auth.use('api').attempt(email, Hash.make(senha), {
+      const token = await auth.use('api').attempt(email, password, {
         name: funcionario[0].email,
+        expiresIn: '7days',
       })
       return { funcionario, token }
     } catch {
@@ -34,11 +31,10 @@ export default class AuthController {
     }
   }
 
-  // public async logout({ auth, response }:HttpContextContract) => {
-  //     await auth.use('api').revoke()
-  //     return {
-  //       revoked: true
-  //     }
-
-  // }
+  public async logout({ auth }: HttpContextContract) {
+    await auth.use('api').revoke()
+    return {
+      revoked: true,
+    }
+  }
 }
